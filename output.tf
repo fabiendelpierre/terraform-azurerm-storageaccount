d01b847b54f701a5e2e8f1f4fd4d2a3e371ad7db
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-01-07 14:40:32 -0500

    add outputs to this module that we're going to need

---
+++ output.tf
@@ -0,0 +1,19 @@
+output "files_endpoint" {
+  value = trimsuffix(trimprefix(azurerm_storage_account.main.primary_file_endpoint, "https://"), "/")
+}
+
+output "primary_access_key" {
+  value = azurerm_storage_account.main.primary_access_key
+}
+
+output "secondary_access_key" {
+  value = azurerm_storage_account.main.secondary_access_key
+}
+
+output "primary_connection_string" {
+  value = azurerm_storage_account.main.primary_connection_string
+}
+
+output "secondary_connection_string" {
+  value = azurerm_storage_account.main.secondary_connection_string
+}
