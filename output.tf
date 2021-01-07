937310e4456d3988f3a08c1b000dd114371e45c5
Fabien Delpierre <fabien.delpierre@lexisnexisrisk.com>
2021-01-07 15:54:54 -0500

    add a couple of outputs that we need for Vault, and clean up unused outputs

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
+output "name" {
+  value = azurerm_storage_account.main.name
+}
+
 output "files_endpoint" {
   value = trimsuffix(trimprefix(azurerm_storage_account.main.primary_file_endpoint, "https://"), "/")
 }
@@ -12,10 +16,6 @@ output "secondary_access_key" {
   sensitive = true
 }
 
-output "primary_connection_string" {
-  value = azurerm_storage_account.main.primary_connection_string
-}
-
-output "secondary_connection_string" {
-  value = azurerm_storage_account.main.secondary_connection_string
+output "storage_share_name" {
+  value = azurerm_storage_share.main.name
 }
